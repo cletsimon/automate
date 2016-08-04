5720f0985f2a19f65ae96157c2ebde6f5ae28f31
cletsimon <cssevillejo@gmail.com>
2016-08-04 11:16:47 +0800

    add project directory

--- release_tag.rb
+++ release_tag.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
-# eg. ruby release_tag.rb "/var/www/cletsimon.github.io" "v0.1"
+# eg. ruby release_tag.rb "github.io" "v0.1"
 # ARGV[0] is app_dir
 # ARGV[1] is Release tag
-Dir.chdir ARGV[0] do
+Dir.chdir "/var/www/" + ARGV[0] do
   puts system "git checkout master"
   puts system "git pull origin master"
   puts system "git tag " + ARGV[1]
